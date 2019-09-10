65e00d75b20b925120bb210d1b7ff1f710c85a63
mrblackheart <mrblackheart@bitbucket.org>
2019-09-10 13:58:32 +0300

    special functions of python

--- hello7.py
+++ hello7.py
@@ -20,4 +20,16 @@ print(l.index(1, 1))
 
 print(l.index(1, 6))
 
-print(l.index(1, 7))
+#print(l.index(1, 7))
+
+l.insert(0,7)
+print(l)
+
+l.pop(0)
+print(l)
+
+l.pop()
+print(l)
+
+l.remove(1)
+print(l)
