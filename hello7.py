88ca44a75271c7f9314b1c17452e69c849b685de
mrblackheart <mrblackheart@bitbucket.org>
2019-09-10 13:57:03 +0300

    special functions of python

--- hello7.py
+++ hello7.py
@@ -1,4 +1,4 @@
-l=[1,2,3,4,5]
+l = [1, 2, 3, 4, 5]
 print(dir(l))
 
 l.append(1)
@@ -9,15 +9,15 @@ print(l.count(1))
 
 print(l.count(7))
 
-l.extend([1,2,3])
+l.extend([1, 2, 3])
 print(l)
-l.append([1,2,3])
+l.append([1, 2, 3])
 print(l)
 
 print(l.index(1))
 
-print(l.index(1,1))
+print(l.index(1, 1))
 
-print(l.index(1,6))
+print(l.index(1, 6))
 
-print(l.index(1,7))
+print(l.index(1, 7))
