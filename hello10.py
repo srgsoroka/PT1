be25e3d09835b1dc38836f35090c3ef6bba00552
mrblackheart <mrblackheart@bitbucket.org>
2019-09-11 12:27:42 +0300

    simple db

--- hello10.py
+++ hello10.py
@@ -1,6 +1,17 @@
+tasks = {}
+
 while True:
     action = input("""a - add
  l - list
  q - quit\n?""").lower()
     if action=='q':
         break
+    elif action == 'a':
+        date=input('Date?')
+        task=input('Task?')
+        tasks[date] = task
+    elif action=='l':
+        date = input('Date?')
+        print(tasks[date])
+
+print(tasks)
