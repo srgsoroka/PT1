fd8751d4e63de4ab9f688db93fac5b27c3762fcd
mrblackheart <mrblackheart@bitbucket.org>
2019-09-12 10:54:37 +0300

    map next

---
+++ hello21.py
@@ -0,0 +1,16 @@
+import functools
+
+
+def add(x, y):
+    """Add x and y"""
+    return x + y
+
+
+m = functools.reduce(add, [1, 2, 3, 4])
+
+print(m)
+
+m = map(lambda x: 2 * x, [1, 2, 3, 4, 5])
+print(m)
+m = list(map(lambda x: 2 * x, [1, 2, 3, 4, 5]))
+print(m)
