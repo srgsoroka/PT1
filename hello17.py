5cecb9ff771a5d96443becb99e0dec51a649f1ea
mrblackheart <mrblackheart@bitbucket.org>
2019-09-12 10:40:11 +0300

    day3

--- hello17.py
+++ hello17.py
@@ -1,9 +1,13 @@
 def add(x, y):
     """Add x and y
     >>> add(2,3)
-    4
-    >>> add(1,-1)
-    0
+    5
+    >>> add(-1,1)
+    Traceback (most recent call last):
+    ...
+    ValueError
     """
+    if x < 0:
+        raise ValueError
     return x + y
 ###doctest with error
---
+++ hello19.py
@@ -0,0 +1,30 @@
+def f(x):
+    return 2 * x
+
+
+print(f(2))
+
+double = f
+
+print(double(3))
+
+
+def execute(f, x):
+    return f(x)
+
+
+print(execute(double, 23))
+
+
+def f():
+    def g(x):
+        return 2 * x
+
+    return g
+
+
+double = f()
+double
+print(double(44))
+
+print(f()(44))
