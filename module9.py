ebefd3b42d9e1d93c7a77a23d208b5d326754687
mrblackheart <mrblackheart@bitbucket.org>
2019-09-12 13:54:46 +0300

    python2 python3

--- module9.py
+++ module9.py
@@ -2,15 +2,21 @@ class Shape:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+import math
 
 # extends
 class Circle(Shape):
     def __init__(self, r, x, y):
         super().__init__(x,y)
         self.r = r
+    def get_area(self):
+        return math.pi*self.r*self.r
 
-
-c = Circle(2, 3)
-
-print(vars(c))
+class Square(Shape):
+    def __init__(self, a, x, y):
+        super().__init__(x,y)
+        self.a = a
+    def get_area(self):
+        return self.a*self.a
+shapes=[Circle(1,1,2),Square(2,3,10),Circle(3,2,5)]
+print(sum(shape.get_area() for shape in shapes))
