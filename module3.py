32c8e84c2e191762f0116e8b27afac7788c8886d
mrblackheart <mrblackheart@bitbucket.org>
2019-09-12 12:37:33 +0300

    map next

--- module3.py
+++ module3.py
@@ -1,8 +1,17 @@
-config='json'
-a=123
+config = 'json'
+a = 123
 if config == 'json':
     import json as serializer
-elif config=='pickle':
+elif config == 'pickle':
     import pickle as serializer
-m=serializer.dumps(a)
+m = serializer.dumps(a)
 print(m)
+import sys
+
+print(sys.path)
+
+sys.path.insert(1,"/tmp")
+print(sys.path)
+
+if __name__ == '__main__':
+    print("Module started")
